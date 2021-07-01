3aad6880ef02f226d34d22aa0c3c2a8d6119b53f
soufianekkk <sou.kabdani@gmail.com>
2021-07-01 16:12:00 +0200

    last commit

--- js.js
+++ js.js
@@ -8,3 +8,20 @@ btn.addEventListener('click',() =>{
         behavior: "smooth"
     })
 })
+
+let page = document.querySelector('html');
+let button = document.getElementById('darkmode')
+
+button.onclick = function(){
+    if (page.classList.contains('dark')){
+        page.classList.remove('dark');
+        button.innerText = 'Mode sombre';
+        button.style.backgroundColor = "dark";
+        
+        
+    }
+    else{
+        page.classList.add('dark');
+        button.innerText = 'Mode clair';  
+    }
+}
